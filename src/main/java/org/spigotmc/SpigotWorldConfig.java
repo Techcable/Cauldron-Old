cadc3896569bed6128678b9a47e73df561a7558c
bloodshot <jdroque@gmail.com>
2014-02-02 03:28:26 -0500

    Reapplied missing Spigot Structure Save patch.

--- src/main/java/org/spigotmc/SpigotWorldConfig.java
+++ src/main/java/org/spigotmc/SpigotWorldConfig.java
@@ -186,16 +186,16 @@ public class SpigotWorldConfig
         log( "Random Lighting Updates: " + randomLightUpdates );
     }
 
-    public boolean saveMineshaftStructureInfo;
-    private void mineshaftStructureInfo()
+    public boolean saveStructureInfo;
+    private void structureInfo()
     {
-        saveMineshaftStructureInfo = getBoolean( "save-mineshaft-structure-info", false );
-        log( "Mineshaft Structure Info Saving: " + saveMineshaftStructureInfo );
-        /*if ( !saveStructureInfo )
+        saveStructureInfo = getBoolean( "save-structure-info", true );
+        log( "Structure Info Saving: " + saveStructureInfo );
+        if ( !saveStructureInfo )
         {
             log( "*** WARNING *** You have selected to NOT save structure info. This may cause structures such as fortresses to not spawn mobs when updating to 1.7!" );
             log( "*** WARNING *** Please use this option with caution, SpigotMC is not responsible for any issues this option may cause in the future!" );
-        }*/
+        }
     }
 
     public int itemDespawnRate;
