0dc77d48481742ddd83189b3af06a8e161d3cf63
bloodshot <jdroque@gmail.com>
2014-08-30 20:11:33 -0400

    Fixed bukkit channel registration for players. Fixes #1621, #1666

--- src/main/java/net/minecraftforge/cauldron/CauldronUtils.java
+++ src/main/java/net/minecraftforge/cauldron/CauldronUtils.java
@@ -135,7 +135,7 @@ public class CauldronUtils {
             byte[] bs = ((net.minecraft.launchwrapper.LaunchClassLoader)CauldronUtils.class.getClassLoader()).getClassBytes("net.minecraft.world.World");
             if (bs != null)
             {
-                FMLRelaunchLog.info("Managed to load a deobfuscated Minecraft name- we are in a deobfuscated environment. Skipping runtime deobfuscation");
+                //FMLRelaunchLog.info("Managed to load a deobfuscated Minecraft name- we are in a deobfuscated environment. Skipping runtime deobfuscation");
                 deobfuscated = true;
             }
         }
